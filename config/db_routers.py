ad7959da08f8f1a94fcffaed8fd9b04037c30037
altair3542 <thiagomesa21@gmail.com>
2025-06-04 20:08:24 -0500

    Agregar configuración de base de datos para 'reportes' y crear enrutador de base de datos

---
+++ config/db_routers.py
@@ -0,0 +1,18 @@
+class ReportesRouter:
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'reportes':
+            return 'reportes'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'reportes':
+            return 'reportes'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == 'reportes':
+            return db == 'reportes'
+        return db == 'default'
